bc0fa0c6c50fe09f03b7c4cc1e2bae2d64303e57
Michael <michaelsteves2001@gmail.com>
2024-08-29 12:46:21 -0400

    fixed homescreen screen issue

--- src/app/homescreen/Homescreen.jsx
+++ src/app/homescreen/Homescreen.jsx
@@ -3,14 +3,16 @@ import Links from './Links';
 
 const Homescreen = () => {
   return (
-    <main className="flex flex-col items-center p-10 sm:m-4 sm:justify-between justify-center sm-2">
-        <div className="">
-            <h1 className="text-[#888E7B] md:text-7xl sm:text-4xl text-3xl">Michael Yang</h1>
-            <div className="flex lg:flex-row flex-col justify-between sm:mt-40 mt-20 items-center">
-                <h2 className="text-[#888E7B] md:text-3xl text-2xl">Dependable</h2>
-                <h2 className="text-[#888E7B] md:text-3xl text-2xl">Analytical</h2>
-                <h2 className="text-[#888E7B] md:text-3xl text-2xl">Developer</h2>
-            </div>
+    <main className="flex flex-col items-center p-10 sm:m-4 justify-center sm-2 w-full">
+        <div className="p-0 w-full flex flex-col items-center">
+          <div className="flex justify-center">
+            <h1 className="text-[#888E7B] md:text-8xl sm:text-6xl text-4xl">Michael Yang</h1>
+          </div>
+          <div className="flex lg:flex-row flex-col justify-between sm:mt-30 mt-20 items-center w-full px-10">
+              <h2 className="text-[#888E7B] md:text-3xl text-2xl">Dependable</h2>
+              <h2 className="text-[#888E7B] md:text-3xl text-2xl">Analytical</h2>
+              <h2 className="text-[#888E7B] md:text-3xl text-2xl">Developer</h2>
+          </div>
         </div>
         <div className="flex w-full sm:mt-80 mt-40">
             <Links />
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import HomeScreen from './homescreen/Homescreen';
 
 export default function Page() {
     return (
-      <main className="flex min-h-screen bg-[#F4FDD9] justify-center items-center sm:min-w-full">
-      <div className="flex sm:my-12 sm:px-10 py-12 mx-2 px-6">
+      <main className="flex min-h-screen bg-[#F4FDD9] justify-center items-center">
+      <div className="sm:my-12 sm:px-8 py-12 mx-2 px-4 w-full">
         <HomeScreen />
       </div>
       </main>
